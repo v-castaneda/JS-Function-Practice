b73cf1f06d2a203185282add6d6a53bf8e049877
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-06-18 02:22:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Your Code Here
+// First decision
 function start() {
   let firstanswer = window.prompt("Do you head left, or right?");
 
@@ -9,22 +9,38 @@ function start() {
   }
 }
 
+// Second round of decisions
 function goLeft() {
   let secondAnswer = window.prompt(`You come across a stray cat. 
     It scampers off down a samll hole, just large enough for you to crawl 
     through. Do you follow it, or continue on your path?`);
-  if (secondAnswer === "back") {
-    start();
+  if (secondAnswer === "follow") {
+   
+  } else if (secondAnswer === 'continue') {
+
   }
 }
 
 function goRight() {
   let secondAnswer = window.prompt(`You come across a snoring dragon. 
-    On the other side of him, you see a shiny chest of treasure. Another path would 
-    lead you away from the dragon all together. Which do you take?`);
-  if (secondAnswer === "back") {
-    start();
+    On the other side of him, you see a shiny chest of treasure. Another 
+    path would lead you away from the dragon all together. Which 
+    do you take?`);
+  if (secondAnswer === "stay") {
+   
+  } else if (secondAnswer === 'run') {
+      
   }
+}
+
+// Third round of decisions
+function goFollow() {
+  let thirdAnswer = window.prompt(
+    `You follow the cat to a colony of cats, snuggled in a fort 
+    of warm blankets and subsisting off of inexplicably warm 
+    soup. They are content with you staying, but you wonder if 
+    you should alert the world to this magical safe haven.`);
+    if 
 }
 
 start();
